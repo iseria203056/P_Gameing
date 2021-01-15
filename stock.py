8704121e917ba24f2ba30702502f953808b2c39d
iseria <74449404+iseria203056@users.noreply.github.com>
2021-01-15 17:50:05 +0900

    Update stock.py

--- stock.py
+++ stock.py
@@ -32,8 +32,8 @@ while True:
     for code in stock:
         url =  "https://finance.yahoo.com/quote/"+code    
         response = rq.get(url) 
-        html_doc = response.text # text 屬性就是 html 檔案
-        soup = BeautifulSoup(response.text, "lxml") # 指定 lxml 作為解析器
+        html_doc = response.text 
+        soup = BeautifulSoup(response.text, "lxml")
         print(code+"  price="+soup.find("span",class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)").string)
     time.sleep(30)
     cls()
